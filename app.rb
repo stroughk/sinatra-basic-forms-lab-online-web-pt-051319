464518e319c646c5fb261b6f7a6b006f3d8b824d
Kateryna Strough <katerynastrough@hotmail.com>
2019-08-15 23:30:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ class App < Sinatra::Base
   get '/' do
      erb :index
    end
- 
+
    get '/new' do
      erb :create_puppy
    end
- 
+
    post '/puppy' do
      @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
      erb :display_puppy
